41be5e61d9b78314880d71f3c06153b84e48094b
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-08-29 08:59:47 -0300

    Trabalhando com state

--- secao2/teste/src/Menu.js
+++ secao2/teste/src/Menu.js
@@ -4,8 +4,12 @@ import { createDrawerNavigator } from 'react-navigation'
 import Simples from './componentes/Simples'
 import ParImpar from './componentes/Parimpar'
 import { Inverter, MegaSena } from './componentes/Multi'
+import Contador from './componentes/Contador'
 
 export default createDrawerNavigator({
+  Contador: {
+    screen: () => <Contador numeroInicial={200}/>
+  },
   MegaSena: {
     screen: () => <MegaSena numeros={7}/>,
     navigatorOptions: { title: 'Mega Sena' }
