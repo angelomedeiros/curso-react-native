670a0dbea46e6768b9336f0773ab3567d6b8ddfa
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-08-29 20:30:55 -0300

    Implementando a logica da calculadora referente aos inputs

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -3,22 +3,39 @@ import {Platform, StyleSheet, Text, View} from 'react-native'
 import Button from './componentes/Button'
 import Display from './componentes/Display'
 
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0
+}
+
 export default class App extends Component {
 
-  state = {
-    displayValue: ''
-  }
+  state = { ...initialState }
 
   addDigit = n => {
-    this.setState({
-      displayValue: this.state.displayValue + n
-    })
+    if ( n === '.' && this.state.displayValue.includes('.') ) {
+      return false
+    }
+
+    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+    const currentValue = clearDisplay ? '' : this.state.displayValue
+    const displayValue = currentValue + n
+    this.setState({ displayValue, clearDisplay: false })
+
+    if ( n !== '!' ) {
+      const newValue = parseFloat(displayValue)
+      const values = [...this.state.values]
+      values[this.state.current] = newValue
+      this.setState({ values })
+    }
+
   }
 
   clearMemory = () => {
-    this.setState({
-      displayValue: '0'
-    })
+    this.setState({ ...initialState })
   }
 
   setOperation = () => {
