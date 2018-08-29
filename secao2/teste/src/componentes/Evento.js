6be1ef9a1f87b8a8fbbe8e23aabd8ef57b76920b
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-08-29 09:48:40 -0300

    Eventos e componentes controlados

---
+++ secao2/teste/src/componentes/Evento.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { View, Text, TextInput } from 'react-native'
+import Padrao from '../estilo/Padrao'
+
+export default class Evento extends Component {
+
+  state = {
+    texto: ''
+  }
+
+  alterarTexto = texto => {
+    this.setState({ texto })
+  }
+
+  render() {
+    return(
+      <View>
+        <TextInput
+          value={this.state.texto}
+          style={Padrao.input}
+          onChangeText={this.alterarTexto} />
+        <Text style={Padrao.font40}>
+          {this.state.texto}
+        </Text>
+      </View>
+    )
+  }
+
+}
