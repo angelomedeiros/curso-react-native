4151ddc08bcbf1ac542e4c05f68a2f222be79445
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-08-29 09:14:02 -0300

    Tratamento entre componentes multiplaforma

--- secao2/teste/index.js
+++ secao2/teste/index.js
@@ -1,6 +1,8 @@
-import { AppRegistry } from 'react-native'
+import { AppRegistry, YellowBox } from 'react-native'
 // import App from './src/App'
 import { name as appName } from './app.json'
 import Menu from './src/Menu'
+
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated'])
 
 AppRegistry.registerComponent(appName, () => Menu)
