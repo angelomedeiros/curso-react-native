f686cea9db96ccc5c5f7f90e720974a2a007abe6
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-08-29 06:05:28 -0300

    Hello world from react native

--- secao2/teste/App.js
+++ secao2/teste/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
 
-export default class App extends React.Component {
-  render() {
+export default class App extends Component {
+  render () {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text style={styles.f20}>
+          Hello World!!!
+        </Text>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
-});
+  f20: {
+    fontSize: 40
+  }
+})
