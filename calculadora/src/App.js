9f8edd0c11ce95d94257753dc0ce5ab9d43cc643
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-08-29 22:02:36 -0300

    Implementacao das operacoes da calculadora

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {Platform, StyleSheet, Text, View} from 'react-native'
+import {StyleSheet, View} from 'react-native'
 import Button from './componentes/Button'
 import Display from './componentes/Display'
 
@@ -38,7 +38,26 @@ export default class App extends Component {
     this.setState({ ...initialState })
   }
 
-  setOperation = () => {
+  setOperation = operation => {
+    if ( this.state.current === 0 ) {
+      this.setState({ operation, current: 1, clearDisplay: true })
+    } else {
+      const equals = operation === '='
+      const values = [...this.state.values]
+      try {
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+      } catch (e) {
+        values[0] = this.state.values[0]
+      }
+      values[1] = 0
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values
+      })
+    }
 
   }
 
